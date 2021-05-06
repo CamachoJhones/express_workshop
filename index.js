ec3e1f40fbe04df8427ebc0a53fed8953ce0df16
CamachoJhones <alonso.ccamacho@gmail.com>
2021-05-05 20:55:23 -0500

    Added UI

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const app = express(); //instancia para mandar llamar al constructor express.
 const pokemon = require('./routes/pokemon')
 const user = require('./routes/user')
 //Middleware
-
 const auth=require('./middleware/auth')
 const notFound=require('./middleware/notFound')
 const index=require('./middleware/index')
+const cors= require('./middleware/cors')
 
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
